15d0374ae3dc1abd56dde1f9a9dd0478adf7252c
danthegoodman1 <xxdanthegoodmanxx@gmail.com>
2018-06-30 19:54:48 -0400

    Create Voltage Code.ino

---
+++ Voltage Code.ino
@@ -0,0 +1,10 @@
+pinMode(A0, INPUT);
+pinMode(D5, OUTPUT);
+digitalWrite(D5, HIGH);
+// This will enable the read, since the 100k Resistor will consume 10uA
+raw = analogRead(A0);
+volt=raw/1023.0;
+digitalWrite(D5, LOW); // Hopefully the volt variable has copied the raw value
+// Otherwise just put the digitalWrite at the bottom
+volt=volt*4.2;
+// Do something with the voltage now\n 
